ecb2c26537f17661abe07e10271a36ffeda27c1c
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-07-12 00:28:49 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+let num1=31
+let num2=2
+
+let multiply=num1*num2
+
+console.log(multiply)
+
+
+let random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+var num3 = 52;
+var num4 = 8;
+var mod = num3%num4
+
+console.log(mod);
+
+
+let set = [16, 7, 20, 18, 8];
+var max = Math.max(...set);
+
+
+
